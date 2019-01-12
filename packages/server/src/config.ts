9a64e93bc6f04efef2579fd74d7f7023112e7b91
Andrew Stalker <andrew@stabtechs.com>
2019-01-11 19:34:32 -0800

    load environment variables with dotenv

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -1,7 +1,14 @@
+import dotenv from 'dotenv-safe'
+
+dotenv.config()
+
+export const env = process.env.NODE_ENV || 'development'
+export const isProd = env === 'production'
+
 export const database = {
   host: process.env.DB_HOST || '0.0.0.0',
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_DATABASE || 'database',
+  username: process.env.DB_USERNAME || '',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_DATABASE || '',
   port: 5432,
 }
