f69ee455bf365d564337a5b77bd4a48216eb9db6
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-02-05 14:24:38 -0800

    refactor: consolidate configs (#28)
    * refactor: consolidate configs
    
    * update
    
    * update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,41 @@
-import jestPlugin from 'eslint-plugin-jest'
-import prettierTypescriptConfig from 'eslint-config-prettier/@typescript-eslint'
-import typescriptPlugin from '@typescript-eslint/eslint-plugin'
+'use strict'
 
-export default {
+const jestPlugin = require('eslint-plugin-jest')
+const prettierTypescriptConfig = require('eslint-config-prettier/@typescript-eslint')
+const typescriptPlugin = require('@typescript-eslint/eslint-plugin')
+
+module.exports = {
   extends: [
     'standard',
+    'standard-react',
     'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:monorepo/recommended',
     'plugin:promise/recommended',
     'plugin:ramda/recommended',
+    'plugin:react/recommended',
     'plugin:unicorn/recommended',
     'prettier',
+    'prettier/react',
     'prettier/standard',
     'prettier/unicorn',
   ],
   overrides: [
     {
-      files: ['**/*.ts', '.*/**/*.ts'],
+      files: ['**/*.ts?(x)', '**/.*/**/*.ts?(x)'],
       parser: typescriptPlugin.configs.recommended.parser,
       plugins: typescriptPlugin.configs.recommended.plugins,
       rules: {
         ...typescriptPlugin.configs.recommended.rules,
-        ...prettierTypescriptConfig.rules,
         'no-use-before-define': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-member-accessibility': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
+        ...prettierTypescriptConfig.rules,
       },
     },
     {
-      files: ['config/jest/**/*.?(js|ts)', 'src/**/?(*.)test.ts'],
+      files: ['packages/*/config/jest/**/*.?(js|ts)', 'packages/*/src/**/?(*.)test.ts?(x)'],
       globals: jestPlugin.environments.globals.globals,
       plugins: jestPlugin.configs.recommended.plugins,
       rules: jestPlugin.configs.recommended.rules,
@@ -38,10 +43,12 @@ export default {
   ],
   plugins: [
     'import',
+    'jsx-a11y',
     'monorepo',
     'prettier',
     'promise',
     'ramda',
+    'react',
     'standard',
     'unicorn',
   ],
--- packages/shared/.eslintrc.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-module.exports = {
-  extends: './node_modules/@fintruth-sdk/scripts/.eslintrc.js',
-}
