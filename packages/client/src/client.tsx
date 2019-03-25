a0bf1df58a055d7f50b6da3339f85f492e9c5fcc
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-03-25 12:28:56 -0700

    chore(deps): bump versions

--- packages/client/src/client.tsx
+++ packages/client/src/client.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import createBrowserHistory from 'history/createBrowserHistory'
 import deepForceUpdate from 'react-deep-force-update'
 import { ApolloProvider } from 'react-apollo'
-import { Location } from 'history'
+import { Location, createBrowserHistory } from 'history'
 import { hydrate } from 'react-dom'
 import { loadableReady } from '@loadable/component'
 
