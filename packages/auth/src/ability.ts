a4d1ff5f2035595ee06442af4385c2c952d195f2
Andrew Stalker <andrew@stabtechs.com>
2019-03-26 14:24:24 -0700

    Add auth module to generate permission rules

---
+++ packages/auth/src/ability.ts
@@ -0,0 +1,14 @@
+import { Ability, AbilityBuilder } from '@casl/ability'
+import { User } from '@fintruth-sdk/shared'
+
+export const defineAbilitiesFor = (user: User) => {
+  const { rules, can } = AbilityBuilder.extract()
+
+  can('create', 'User')
+
+  if (user.isAdmin) {
+    can('manage', 'all')
+  }
+
+  return new Ability(rules)
+}
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,1 @@
+export { defineAbilitiesFor } from './ability'
