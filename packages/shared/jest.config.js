24aac7e9ffa21b0914890434a025b3fd6eb99889
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-03-28 13:17:20 -0700

    feat: integrate codecov

---
+++ packages/shared/jest.config.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const { defaults } = require('jest-config')
+
+module.exports = {
+  collectCoverageFrom: ['src/**/*.ts'],
+  errorOnDeprecated: true,
+  moduleDirectories: [...defaults.moduleDirectories, '<rootDir>/src'],
+  restoreMocks: true,
+  testMatch: ['<rootDir>/src/**/?(*.)test.ts'],
+  testRunner: 'jest-circus/runner',
+  transform: { '^.+\\.ts$': require.resolve('ts-jest') },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.js$'],
+}
