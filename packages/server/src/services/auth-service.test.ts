07c312705d6b20667c4ff40b6f858a6621cd8cfa
Andrew Stalker <andrew@stabtechs.com>
2019-04-07 10:59:04 -0700

    test confirm two factor

--- packages/server/src/services/auth-service.test.ts
+++ packages/server/src/services/auth-service.test.ts
@@ -1,27 +1,31 @@
-import { equals } from 'ramda'
+import { equals, F, T } from 'ramda'
 import { Container } from 'typedi'
 
+import { User } from '../entities'
+import { Response, ResponseError } from '../resolvers/types'
 import AuthService from './auth-service'
 
 jest.mock('typeorm-typedi-extensions', () => ({
   InjectRepository: () => () => {},
 }))
 
-const userMock = {
-  email: 'test@test.com',
-  validatePassword: jest.fn(equals('good')),
-}
+jest.mock('type-graphql', () => ({
+  Field: () => () => {},
+  InputType: () => () => {},
+  ObjectType: () => () => {},
+}))
 
-const userRepositoryMock: any = {
+const getUserRepositoryMock: any = (userMock?: Partial<User>) => ({
   findOne: jest.fn(() => Promise.resolve(userMock)),
-}
+  update: jest.fn(() => Promise.resolve(true)),
+})
 
 describe('AuthService', () => {
   let service: AuthService
 
   beforeEach(() => {
     service = Container.get(AuthService)
-    service.userRepository = userRepositoryMock
+    service.userRepository = getUserRepositoryMock()
   })
 
   afterEach(() => {
@@ -34,17 +38,68 @@ describe('AuthService', () => {
 
   describe('authenticate', () => {
     it('should return a user using a correct password', async () => {
+      const user = {
+        validatePassword: jest.fn(equals('good')),
+      }
+
+      service.userRepository = getUserRepositoryMock(user)
+
       const result = await service.authenticate('test@test.com', 'good')
 
-      expect(userMock.validatePassword).toHaveBeenCalledWith('good')
-      expect(result).toStrictEqual(userMock)
+      expect(user.validatePassword).toHaveBeenCalledWith('good')
+      expect(result).toStrictEqual(user)
     })
 
     it('should return null using an incorrect password', async () => {
+      const user = {
+        validatePassword: jest.fn(equals('good')),
+      }
+
+      service.userRepository = getUserRepositoryMock(user)
+
       const result = await service.authenticate('test@test.com', 'bad')
 
-      expect(userMock.validatePassword).toHaveBeenCalledWith('bad')
+      expect(user.validatePassword).toHaveBeenCalledWith('bad')
       expect(result).toBeNull()
+    })
+  })
+
+  describe('confirmTwoFactorAuth', () => {
+    it('should update user secret using a valid token', async () => {
+      const user = {
+        secretTemp: 'secret',
+      }
+
+      service.userRepository = getUserRepositoryMock(user)
+      service.verifyTwoFactorAuthToken = T
+
+      const result = await service.confirmTwoFactorAuth('token', 'test')
+
+      expect(service.userRepository.update).toHaveBeenCalledWith('test', {
+        secret: 'secret',
+        secretTemp: undefined,
+      })
+      expect(result).toStrictEqual(new Response())
+    })
+
+    it('should return a failure response using an invalid token', async () => {
+      const user = {
+        secretTemp: 'secret',
+      }
+
+      service.userRepository = getUserRepositoryMock(user)
+      service.verifyTwoFactorAuthToken = F
+
+      const result = await service.confirmTwoFactorAuth('token', 'test')
+
+      const error = new ResponseError('Token is invalid or expired')
+      error.id = expect.any(String)
+
+      expect(result).toStrictEqual(
+        new Response({
+          error,
+        })
+      )
     })
   })
 })
