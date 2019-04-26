c5d8fbf6d60f1e9d942e1e03a25a32a1dc96de9f
Andrew Stalker <andrew@stabtechs.com>
2019-04-26 12:22:10 -0700

    Update eslint configuration (#99)

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,10 @@ module.exports = {
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-member-accessibility': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          { argsIgnorePattern: "^_" },
+        ],
         ...prettierTypescriptConfig.rules,
       },
     },
