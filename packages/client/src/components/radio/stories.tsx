173265ccf78c5034d83cbb4cc6d6a20fd0c18fb0
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-05-01 12:46:40 -0700

    update

--- packages/client/src/components/radio/stories.tsx
+++ packages/client/src/components/radio/stories.tsx
@@ -13,23 +13,21 @@ storiesOf('Components|Radio ', module)
   .addDecorator(centered)
   .add('Default', () => (
     <Formik initialValues={initialValues} onSubmit={action('On Submit')}>
-      {() => (
-        <React.Fragment>
-          <Radio
-            id="f524dabc-13b6-4fa2-b633-2a852b28b39a"
-            isDisabled={boolean('Is Disabled (Radio A)', false, 'Radio A')}
-            label={text('Label (Radio A)', 'Radio A', 'Radio A')}
-            name="radioGroup"
-            value="radio-a"
-          />
-          <Radio
-            id="53394530-d091-4a93-be90-351df19d704c"
-            isDisabled={boolean('Is Disabled (Radio B)', false, 'Radio B')}
-            label={text('Label (Radio B)', 'Radio B', 'Radio B')}
-            name="radioGroup"
-            value="radio-b"
-          />
-        </React.Fragment>
-      )}
+      <React.Fragment>
+        <Radio
+          id="f524dabc-13b6-4fa2-b633-2a852b28b39a"
+          isDisabled={boolean('Is Disabled (Radio A)', false, 'Radio A')}
+          label={text('Label (Radio A)', 'Radio A', 'Radio A')}
+          name="radioGroup"
+          value="radio-a"
+        />
+        <Radio
+          id="53394530-d091-4a93-be90-351df19d704c"
+          isDisabled={boolean('Is Disabled (Radio B)', false, 'Radio B')}
+          label={text('Label (Radio B)', 'Radio B', 'Radio B')}
+          name="radioGroup"
+          value="radio-b"
+        />
+      </React.Fragment>
     </Formik>
   ))
