8dc284f26af6d5c1c010bcf2f1f351d36c116e61
Andrew Stalker <andrew@stabtechs.com>
2019-04-30 20:36:35 -0700

    update config

--- packages/server/src/services/config-service.ts
+++ packages/server/src/services/config-service.ts
@@ -10,6 +10,17 @@ export default class ConfigService {
     port: Number(process.env.APP_PORT || '4000'),
     trustProxy: process.env.TRUST_PROXY || 'loopback',
   }
+  aws = {
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY || '',
+      secretAccessKey: process.env.AWS_ACCESS_SECRET || '',
+    },
+    region: process.env.AWS_REGION,
+    ses: {
+      apiVersion: '2010-12-01',
+      sender: 'noreply@fintruth.com',
+    },
+  }
   env = process.env.NODE_ENV || 'development'
   isProd = this.env === 'production'
 }
