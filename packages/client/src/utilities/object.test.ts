571ca0cd802d9144edc5e8b7e2986e2977e77b2d
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-05-16 20:02:15 -0700

    update

--- packages/client/src/utilities/object.test.ts
+++ packages/client/src/utilities/object.test.ts
@@ -1,34 +1,36 @@
 import { omitDeep } from './object'
 
-test('The omitDeep function should return an unmodified object', () => {
-  const date = new Date()
-  const result = omitDeep([], date)
+describe('omitDeep', () => {
+  test('should return an unmodified object', () => {
+    const date = new Date()
+    const result = omitDeep([], date)
 
-  expect(result).toStrictEqual(date)
-})
-
-test('The omitDeep function should return an object with the nested keys removed', () => {
-  const result = omitDeep(['a', 'c'], { a: 'a', b: 'b', c: 'c' })
-
-  expect(result).toStrictEqual({ b: 'b' })
-})
-
-test('The omitDeep function should return an object with the deeply nested keys removed', () => {
-  const result = omitDeep(['b', 'd', 'f'], {
-    a: 'a',
-    b: 'b',
-    c: { b: 'b', d: 'd', e: { b: 'b', f: 'f', g: { b: 'b', c: 'c' } } },
+    expect(result).toStrictEqual(date)
   })
 
-  expect(result).toStrictEqual({ a: 'a', c: { e: { g: { c: 'c' } } } })
-})
+  test('should return an object with the nested keys removed', () => {
+    const result = omitDeep(['a', 'c'], { a: 'a', b: 'b', c: 'c' })
 
-test('The omitDeep function should return an array with the deeply nested keys removed', () => {
-  const date = new Date()
-  const result = omitDeep(
-    ['b'],
-    [{ a: 'a', b: 'b' }, date, [{ a: 'a', b: 'b' }]]
-  )
+    expect(result).toStrictEqual({ b: 'b' })
+  })
 
-  expect(result).toStrictEqual([{ a: 'a' }, date, [{ a: 'a' }]])
+  test('should return an object with the deeply nested keys removed', () => {
+    const result = omitDeep(['b', 'd', 'f'], {
+      a: 'a',
+      b: 'b',
+      c: { b: 'b', d: 'd', e: { b: 'b', f: 'f', g: { b: 'b', c: 'c' } } },
+    })
+
+    expect(result).toStrictEqual({ a: 'a', c: { e: { g: { c: 'c' } } } })
+  })
+
+  test('should return an array with the deeply nested keys removed', () => {
+    const date = new Date()
+    const result = omitDeep(
+      ['b'],
+      [{ a: 'a', b: 'b' }, date, [{ a: 'a', b: 'b' }]]
+    )
+
+    expect(result).toStrictEqual([{ a: 'a' }, date, [{ a: 'a' }]])
+  })
 })
