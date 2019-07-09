30b972d8df833f830685419dd440391bffac736f
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-07-08 19:46:35 -0700

    chore(deps): bump versions

--- packages/client/types/formik/index.d.ts
+++ packages/client/types/formik/index.d.ts
@@ -1,5 +1,7 @@
-import 'formik'
+import { FormikFormProps } from 'formik'
 
 declare module 'formik' {
   export type Validate = (value: any) => string | Promise<void> | undefined
+
+  export function Form(props: FormikFormProps): JSX.Element
 }
