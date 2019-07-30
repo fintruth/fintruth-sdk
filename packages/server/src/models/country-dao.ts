912b603304b2f5d9177a8ceace83ca61d497b1da
Andrew Stalker <andrew@stabtechs.com>
2019-07-30 11:06:43 -0700

    update

--- packages/server/src/models/country-dao.ts
+++ packages/server/src/models/country-dao.ts
@@ -1,10 +1,8 @@
-import { Service } from 'typedi'
 import { EntityRepository, Repository } from 'typeorm'
 
 import { Country } from '../entities'
 
 @EntityRepository(Country)
-@Service()
 export default class CountryDao extends Repository<Country> {
   findById(id: string) {
     return this.findOne(id)
