9420ce486dce8a36193dfbd3457791c3f904e564
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-09-23 11:51:33 -0700

    update

--- packages/client/.storybook/webpack.config.js
+++ packages/client/.storybook/webpack.config.js
@@ -77,7 +77,10 @@ module.exports = ({ config, mode }) => {
     },
     plugins: [
       ...config.plugins,
-      new DefinePlugin({ __IS_DEV__: isDev, 'process.env.BROWSER_ENV': `'${mode}'` }),
+      new DefinePlugin({
+        __IS_DEV__: isDev,
+        'process.env.BROWSER_ENV': `'${mode}'`,
+      }),
     ],
     resolve: {
       ...config.resolve,
