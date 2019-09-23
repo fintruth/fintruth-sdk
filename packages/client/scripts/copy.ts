c98b36dc41fccd96e14f7af11230f7e8e58aab3f
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-09-23 13:45:13 -0700

    feat(client): add extract-translations script (#976)

--- packages/client/scripts/copy.ts
+++ packages/client/scripts/copy.ts
@@ -18,17 +18,23 @@ const config = JSON.stringify(
   2
 )
 
+const isWatch = process.argv.includes('--watch')
+
 const copy = async () => {
   await makeDir('build')
 
   await Promise.all([
     copyDir('public', 'build/public'),
+    copyDir('src/translations/locales', 'build/locales'),
     copyFile('../../yarn.lock', 'build/yarn.lock'),
     writeFile('build/package.json', config),
   ])
 
-  if (process.argv.includes('--watch')) {
-    const watcher = chokidar.watch(['public/**/*'], { ignoreInitial: true })
+  if (isWatch) {
+    const watcher = chokidar.watch(
+      ['public/**/*', 'src/translations/locales/**/*'],
+      { ignoreInitial: true }
+    )
 
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
     watcher.on('all', async (eventName, path) => {
--- packages/client/src/config.ts
+++ packages/client/src/config.ts
@@ -1,5 +1,7 @@
 if (process.env.BROWSER_ENV) {
-  throw new Error('Do not import "config.ts" from inside the client-side code.')
+  throw new Error("Do not import 'config.ts' from inside the client-side code.")
 }
+
+export const locales = ['en']
 
 export const port = Number(process.env.PORT) || 3000
