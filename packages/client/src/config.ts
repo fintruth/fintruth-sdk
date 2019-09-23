9604de99d7d34248d1c89853c4073e0c4ab70f68
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-09-23 15:30:33 -0700

    feat(client): add graphql capabilities to server (#984)

--- packages/client/src/config.ts
+++ packages/client/src/config.ts
@@ -1,7 +1,12 @@
+import { join, resolve } from 'path'
+
 if (process.env.BROWSER_ENV) {
   throw new Error("Do not import 'config.ts' from inside the client-side code.")
 }
 
 export const locales = ['en']
+
+export const rootDir = resolve(__dirname)
+export const publicDir = join(rootDir, 'public')
 
 export const port = Number(process.env.PORT) || 3000
