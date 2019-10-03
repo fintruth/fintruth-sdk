3b39b7f341bfd71b93ee1621e87ff0b6a378f11f
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-10-03 14:35:41 -0700

    refactor(client): remove unused assets

--- packages/client/src/assets/stories.tsx
+++ packages/client/src/assets/stories.tsx
@@ -4,16 +4,12 @@ import React from 'react'
 import ExpandLess from './expand-less.svg'
 import ExpandMore from './expand-more.svg'
 import logoUrl from './logo.png'
-import SortDown from './sort-down.svg'
-import SortUp from './sort-up.svg'
 import UserCircle from './user-circle.svg'
 import Assets from '.'
 
 const icons = [
   { Icon: ExpandLess, label: 'Expand Less' },
   { Icon: ExpandMore, label: 'Expand More' },
-  { Icon: SortDown, label: 'Sort Down' },
-  { Icon: SortUp, label: 'Sort Up' },
   { Icon: UserCircle, label: 'User Circle' },
 ]
 
